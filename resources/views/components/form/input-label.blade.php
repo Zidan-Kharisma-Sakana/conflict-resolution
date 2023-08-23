e4033278c85f7866c96847aae4f03fbb558eebb5
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-08-23 10:27:43 +0700

    views: update components & layouts

--- resources/views/components/form/input-label.blade.php
+++ resources/views/components/form/input-label.blade.php
@@ -1,5 +1,14 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-    {{ $value ?? $slot }}
-</label>
+@if (isset($attributes['required']))
+    <div class="flex gap-x-1">
+        <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+            {{ $value ?? $slot }}
+        </label>
+        <p class="text-red-500">*</p>
+    </div>
+@else
+    <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+        {{ $value ?? $slot }}
+    </label>
+@endif
