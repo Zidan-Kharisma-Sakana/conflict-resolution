d6dbd0ec37c272eebe290e4dc998f07cf169a2c8
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-08-30 17:54:23 +0700

    chore: add notif model

---
+++ app/Http/Controllers/NotifikasiController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreNotifikasiRequest;
+use App\Http\Requests\UpdateNotifikasiRequest;
+use App\Models\Notifikasi;
+use Illuminate\Http\Request;
+
+class NotifikasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Notifikasi $notifikasi)
+    {
+        //
+    }
+}
