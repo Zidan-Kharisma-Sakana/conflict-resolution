e8dcc28ab0de6d8eb5316a58646d379313cc86f5
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-08-31 08:26:51 +0700

    feat: create notification interface

---
+++ app/Interfaces/NotificationServiceInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Models\Complaint\Kesepakatan;
+use App\Models\Complaint\Mediasi;
+use App\Models\Complaint\Musyawarah;
+use App\Models\Complaint\Pengaduan;
+use App\Models\User;
+
+interface NotificationServiceInterface
+{
+    public function pengaduanCreated(Pengaduan $pengaduan);
+    public function pengaduanApproved(Pengaduan $pengaduan);
+    public function pengaduanRejected(Pengaduan $pengaduan);
+    public function pengaduanCancelled(Pengaduan $pengaduan);
+    public function pengaduanClosed(Pengaduan $pengaduan);
+    public function musyawarahCreated(Musyawarah $musyawarah);
+    public function mediasiCreated(Mediasi $mediasi);
+    public function kesepakatanCreated(Kesepakatan $kesepakatan);
+}
