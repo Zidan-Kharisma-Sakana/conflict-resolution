a0ec2bd0b2030904f4cb8720a42cb388b1e21638
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-09-05 16:30:08 +0700

    fix: missing import

--- app/Models/Profile/Bursa.php
+++ app/Models/Profile/Bursa.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Profile;
 
+use App\Models\Complaint\Pengaduan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
